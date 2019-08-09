4f73d968608778ad3757ad5fcca976d1c8d3a54a
unknown <jasoncollum@gmail.com>
2019-08-09 10:28:03 -0700

    query 7-12

--- ChinookQueries.sql
+++ ChinookQueries.sql
@@ -28,9 +28,53 @@
 -- 6. sales_agent_invoices.sql: 
 -- Provide a query that shows the invoices associated with each sales agent. 
 -- The resultant table should include the Sales Agent's full name.
--- SELECT i.InvoiceId, i.CustomerId, i.InvoiceDate, i.BillingAddress, i.BillingCity, i.BillingState, i.BillingCountry,
---     i.BillingPostalCode, i.Total, e.FirstName, e.LastName
---     FROM Invoice i
---     JOIN Employee e ON i.BillingCountry = e.Country;
+-- SELECT e.FirstName, 
+--     e.LastName,
+--     i.InvoiceId
+--     FROM Employee e
+--     JOIN Customer c ON c.SupportRepId = e.EmployeeId
+--     JOIN Invoice i ON i.CustomerId = c.CustomerId;
 
-7.
+-- 7. invoice_totals.sql: 
+-- Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name 
+-- for all invoices and customers.
+-- SELECT i.Total as InvoiceTotal, c.FirstName as CustomerFirstName, c.LastName as CustomerLastName, c.Country, e.FirstName as AgentFirstName, e.LastName as AgentLastName
+-- FROM Invoice i
+-- JOIN Customer c ON i.CustomerId = c.CustomerId
+-- JOIN Employee e ON c.SupportRepId = e.EmployeeId
+-- WHERE e.Title = 'Sales Support Agent';
+
+-- 8. total_invoices_{year}.sql: 
+-- How many Invoices were there in 2009 and 2011?
+-- SELECT COUNT(*)
+-- FROM Invoice
+-- WHERE InvoiceDate BETWEEN '2009-01-01' AND '2010-01-01'
+-- OR InvoiceDate BETWEEN '2011-01-01' AND '2012-01-01';
+
+-- 9. total_sales_{year}.sql: 
+-- What are the respective total sales for each of those years?
+-- SELECT SUM(CASE WHEN InvoiceDate BETWEEN '2009' AND '2010' THEN Total END) AS Total2009, 
+--     SUM(CASE WHEN InvoiceDate BETWEEN '2011' AND '2012' THEN Total END) AS Total2011
+-- FROM Invoice;
+
+-- 10. invoice_37_line_item_count.sql: 
+-- Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37.
+-- SELECT COUNT(*) FROM InvoiceLine
+-- WHERE InvoiceId = 37;
+
+-- 11. line_items_per_invoice.sql: 
+-- Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. 
+-- HINT: GROUP BY
+-- SELECT COUNT(*) AS LinedItemsCount, InvoiceId
+-- FROM InvoiceLine
+-- GROUP BY InvoiceId;
+
+-- 12. line_item_track.sql: 
+-- Provide a query that includes the purchased track name with each invoice line item.
+-- SELECT il.InvoiceLineId, t.Name AS TrackName
+-- FROM InvoiceLine il
+-- JOIN Track t ON t.TrackId = il.TrackId
+-- ORDER BY il.InvoiceLineId ASC;
+
+-- 13. line_item_track_artist.sql: 
+-- Provide a query that includes the purchased track name AND artist name with each invoice line item.
